9fbbd5451cf5b2edc2677ad9b35a974c6b6a3fa3
sdrozzy <sdrozzy@gmail.com>
2014-11-01 11:14:01 -0700

    added icons and depo view controller

--- Depo/DepoViewController.h
+++ Depo/DepoViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  DepoViewController.h
 //  Depo
 //
 //  Created by Samuel Drozdov on 11/1/14.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface DepoViewController : UIViewController
 
 @end
-
