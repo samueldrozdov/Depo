7a514ba8d6b043fb1dc0180882f74244199aaa61
phillipou94 <phillipou@Phillips-MacBook-Pro.local>
2014-11-01 22:41:44 -0700

    lets see if this works

--- transaction.js
+++ transaction.js
@@ -4,15 +4,11 @@ var bitcoin = require('bitcoinjs-lib');
 // The Chain API will never accept your private key.
 // Keep the private key stored in a safe place alongside
 // your program.  
-var privateKey = "044015ed16d308b54d5b213324abb3c3c7e54339b8cbda040fd4f7a1cf2e9c77aff26947bac96f7de1ac327131ea24ff3d8bbba85c9f336873b43c154005e4f678";
-var wif = privateKey.toWIF()
-console.log('wif' + wif);
-
-var key = new bitcoin.ECKey.fromWIF(wif)
+var key = new bitcoin.ECKey.fromWIF("Your private key in WIF format.")
 var txn = new bitcoin.Transaction()
 // We use a previous transaction where our private key
 // can authorize the use of its output at index 1.
-txn.addInput("dedbeb80d4b624f75445897e7f7c39b07861cdaaa2da30ea88499c705ebc893c", 1);
+txn.addInput("Previous transaction hash.", 1);
 // Specify the address hash of where our bitcoin should go.
 // We are sending 10,000 Satoshi to a friend.
 txn.addOutput("n2Q1BDERQ7voY4uzUYUQZNdHUsFwfw59ze", 10000)
